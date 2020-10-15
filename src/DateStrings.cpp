85d76294338e91f7ea5f0db11d66769ff778c54d
James Foster <github@jgfoster.net>
2020-10-14 22:05:52 -0700

    Format using clang-format.

--- src/DateStrings.cpp
+++ src/DateStrings.cpp
@@ -3,17 +3,19 @@
  *
  * Updated for Arduino 1.5.7 18 July 2014
  *
- * No memory is consumed in the sketch if your code does not call any of the string methods
- * You can change the text of the strings, make sure the short strings are each exactly 3 characters 
- * the long strings can be any length up to the constant dt_MAX_STRING_LEN defined in TimeLib.h
- * 
+ * No memory is consumed in the sketch if your code does not call any of the
+ * string methods You can change the text of the strings, make sure the short
+ * strings are each exactly 3 characters the long strings can be any length up
+ * to the constant dt_MAX_STRING_LEN defined in TimeLib.h
+ *
  */
 
 #include <Arduino.h>
 
-// Arduino.h should properly define PROGMEM, PGM_P, strcpy_P, pgm_read_byte, pgm_read_ptr
-// But not all platforms define these as they should.  If you find a platform needing these
-// defined, or if any this becomes unnecessary as platforms improve, please send a pull req.
+// Arduino.h should properly define PROGMEM, PGM_P, strcpy_P, pgm_read_byte,
+// pgm_read_ptr But not all platforms define these as they should.  If you find
+// a platform needing these defined, or if any this becomes unnecessary as
+// platforms improve, please send a pull req.
 #if defined(ESP8266)
 #undef PROGMEM
 #define PROGMEM
@@ -21,11 +23,11 @@
 
 #include "TimeLib.h"
 
- 
 // the short strings for each day or month must be exactly dt_SHORT_STR_LEN
-#define dt_SHORT_STR_LEN  3 // the length of short strings
+#define dt_SHORT_STR_LEN 3 // the length of short strings
 
-static char buffer[dt_MAX_STRING_LEN+1];  // must be big enough for longest string and the terminating null
+static char buffer[dt_MAX_STRING_LEN + 1]; // must be big enough for longest
+                                           // string and the terminating null
 
 const char monthStr0[] PROGMEM = "";
 const char monthStr1[] PROGMEM = "January";
@@ -41,13 +43,13 @@ const char monthStr10[] PROGMEM = "October";
 const char monthStr11[] PROGMEM = "November";
 const char monthStr12[] PROGMEM = "December";
 
-const PROGMEM char * const PROGMEM monthNames_P[] =
-{
-    monthStr0,monthStr1,monthStr2,monthStr3,monthStr4,monthStr5,monthStr6,
-    monthStr7,monthStr8,monthStr9,monthStr10,monthStr11,monthStr12
-};
+const PROGMEM char *const PROGMEM monthNames_P[] = {
+    monthStr0,  monthStr1,  monthStr2, monthStr3, monthStr4,
+    monthStr5,  monthStr6,  monthStr7, monthStr8, monthStr9,
+    monthStr10, monthStr11, monthStr12};
 
-const char monthShortNames_P[] PROGMEM = "ErrJanFebMarAprMayJunJulAugSepOctNovDec";
+const char monthShortNames_P[] PROGMEM =
+    "ErrJanFebMarAprMayJunJulAugSepOctNovDec";
 
 const char dayStr0[] PROGMEM = "Err";
 const char dayStr1[] PROGMEM = "Sunday";
@@ -58,40 +60,35 @@ const char dayStr5[] PROGMEM = "Thursday";
 const char dayStr6[] PROGMEM = "Friday";
 const char dayStr7[] PROGMEM = "Saturday";
 
-const PROGMEM char * const PROGMEM dayNames_P[] =
-{
-   dayStr0,dayStr1,dayStr2,dayStr3,dayStr4,dayStr5,dayStr6,dayStr7
-};
+const PROGMEM char *const PROGMEM dayNames_P[] = {
+    dayStr0, dayStr1, dayStr2, dayStr3, dayStr4, dayStr5, dayStr6, dayStr7};
 
 const char dayShortNames_P[] PROGMEM = "ErrSunMonTueWedThuFriSat";
 
 /* functions to return date strings */
 
-char* monthStr(uint8_t month)
-{
-    strcpy_P(buffer, (PGM_P)pgm_read_ptr(&(monthNames_P[month])));
-    return buffer;
+char *monthStr(uint8_t month) {
+  strcpy_P(buffer, (PGM_P)pgm_read_ptr(&(monthNames_P[month])));
+  return buffer;
 }
 
-char* monthShortStr(uint8_t month)
-{
-   for (int i=0; i < dt_SHORT_STR_LEN; i++)      
-      buffer[i] = pgm_read_byte(&(monthShortNames_P[i+ (month*dt_SHORT_STR_LEN)]));  
-   buffer[dt_SHORT_STR_LEN] = 0;
-   return buffer;
+char *monthShortStr(uint8_t month) {
+  for (int i = 0; i < dt_SHORT_STR_LEN; i++)
+    buffer[i] =
+        pgm_read_byte(&(monthShortNames_P[i + (month * dt_SHORT_STR_LEN)]));
+  buffer[dt_SHORT_STR_LEN] = 0;
+  return buffer;
 }
 
-char* dayStr(uint8_t day) 
-{
-   strcpy_P(buffer, (PGM_P)pgm_read_ptr(&(dayNames_P[day])));
-   return buffer;
+char *dayStr(uint8_t day) {
+  strcpy_P(buffer, (PGM_P)pgm_read_ptr(&(dayNames_P[day])));
+  return buffer;
 }
 
-char* dayShortStr(uint8_t day) 
-{
-   uint8_t index = day*dt_SHORT_STR_LEN;
-   for (int i=0; i < dt_SHORT_STR_LEN; i++)      
-      buffer[i] = pgm_read_byte(&(dayShortNames_P[index + i]));  
-   buffer[dt_SHORT_STR_LEN] = 0; 
-   return buffer;
+char *dayShortStr(uint8_t day) {
+  uint8_t index = day * dt_SHORT_STR_LEN;
+  for (int i = 0; i < dt_SHORT_STR_LEN; i++)
+    buffer[i] = pgm_read_byte(&(dayShortNames_P[index + i]));
+  buffer[dt_SHORT_STR_LEN] = 0;
+  return buffer;
 }
