05ed4cf048238c473b1f2167c533ce51ec9acc9a
eucalvo <eliamcalvo24@gmail.com>
2020-10-18 23:48:21 -0700

    tests for time library

--- test/test.cpp
+++ test/test.cpp
@@ -2,9 +2,50 @@
 #include "ArduinoUnitTests.h"
 #include "TimeLib.h"
 
-unittest(test) {
-  assertTrue(true);
+// hour();            // the hour now  (0-23)
+// minute();          // the minute now (0-59)
+// second();          // the second now (0-59)
+// day();             // the day now (1-31)
+// weekday();         // day of the week (1-7), Sunday is day 1
+// month();           // the month now (1-12)
+// year();            // the full four digit year: (2009, 2010 etc)
+// hourFormat12();    // the hour now in 12 hour format
+// isAM();            // returns true if time now is AM
+// isPM();            // returns true if time now is PM
+// now();             // returns the current time as seconds since Jan 1 1970
+unittest(DefaultNow) {
+  assertTrue(hour() == 0);
+  assertTrue(minute() == 0);
+  assertTrue(second() == 0);
+  assertTrue(day() == 0);
+  assertTrue(month() == 0);
+  assertTrue(year() == 1970);
+  assertTrue(isAM() == true);
+  assertTrue(isPM() == false);
+  assertTrue(now() == 0);
+  // assertTrue(hourFormat12() == 0);
+}
+
+// time_t t = now(); // store the current time in time variable t
+// hour(t);          // returns the hour for the given time t
+// minute(t);        // returns the minute for the given time t
+// second(t);        // returns the second for the given time t
+// day(t);           // the day for the given time t
+// weekday(t);       // day of the week for the given time t
+// month(t);         // the month for the given time t
+// year(t);          // the year for the given time t
+unittest(SpecifiedTime) {
+  time_t t = 50 * 365 * 24 * 60 * 60;
+  std::cout << t << " " << year(t) << "-" << month(t) << "-" << day(t)
+            << std::endl;
+  assertTrue(year(t) == 2019);
+  assertTrue(hour(t) == 0);
+  assertTrue(minute(t) == 0);
+  assertTrue(second(t) == 0);
+  assertTrue(day(t) == 20);
+  assertTrue(month(t) == 12);
+  // assertTrue(weekday(t) == 0);
 }
 
 unittest_main()
-// bundle exec arduino_ci_remote.rb --skip-compilation
+    // bundle exec arduino_ci_remote.rb --skip-compilation
