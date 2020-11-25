0c48521fc9bf3b3c6de70de3af71a2be84052450
James Foster <github@jgfoster.net>
2020-11-25 14:09:24 -0800

    Use published `arduino_ci` gem (currently version 0.4.0) (#5)
    * Update formatting on examples.
    * Fix test script
    * Disable compiling examples since they seem to have a tangle of undocumented dependencies.

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -2,10 +2,14 @@
 
 # set up directories
 bundle exec ensure_arduino_installation.rb
+cd $(bundle exec arduino_library_location.rb)
 
-# get required libraries
-git clone https://github.com/PaulStoffregen/DS1307RTC.git
-mv DS1307RTC $(bundle exec arduino_library_location.rb)/DS1307RTC
+# add missing libraries
+export GITHUB="https://github.com/Arduino-CI"
+if [ ! -d "./DS1307RTC" ] ; then
+  git clone $SHALLOW https://github.com/PaulStoffregen/DS1307RTC.git
+fi
 
-git clone https://github.com/MarkusLange/Arduino-Due-RTC-Library.git
-mv Arduino-Due-RTC-Library $(bundle exec arduino_library_location.rb)/rtc_clock
+if [ ! -d "./Arduino-Due-RTC-Library.git" ] ; then
+  git clone $SHALLOW https://github.com/MarkusLange/Arduino-Due-RTC-Library.git
+fi
--- scripts/testAndBuild.sh
+++ scripts/testAndBuild.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 bundle config --local path vendor/bundle
 bundle install
-bundle exec arduino_ci_remote.rb
+bundle exec arduino_ci.rb
